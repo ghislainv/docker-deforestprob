33bf2a6dbfc2272599e1c489213b0acdf541431f
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2017-08-27 13:53:28 +0200

    Set Jupyter password

--- run_ipython.sh
+++ run_ipython.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-jupyter-notebook --no-browser --port 8888 --ip=0.0.0.0 --NotebookApp.token=''
+jupyter-notebook --no-browser --port 8888 --ip=0.0.0.0 --NotebookApp.password='sha1:ccba7d6005b3:ec46325bd3303fbab7699ea0eaf29eaf2edf2218'
